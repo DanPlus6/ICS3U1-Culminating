8f70c96d6f2d5baef160bd75aa5d96603c58e2e5
Ian Choy <boidiamond69420@gmail.com>
2026-06-10 22:27:03 +0800

    Added skeleton code to test wiresTask

--- assets/script/Day 2/wiresTask.js
+++ assets/script/Day 2/wiresTask.js
@@ -1,2 +1,37 @@
+/**
+ * By Ian Choy
+ */
+
 'use strict';
 
+//get the Canvas from the HTML
+const CANVAS = document.getElementById('game-canvas');
+
+//get the graphics content from the Canvas for drawing directions
+const BRUSH = CANVAS.getContext('2d');
+
+//information about the circle
+let circleX = 95;
+let circleY = 50;
+let circleRadius = 40;
+
+/**
+ * Adds all event listeners to the game
+ */
+function addEventListeners(){
+    document.body.addEventListener('mousemove', checkMouseMove);
+}
+
+/**
+ * 
+ * @param {MouseEvent} mouseEvent information about the mouse position
+ */
+function checkMouseMove(mouseEvent){
+    console.log(`Mouse X: ${mouseEvent.clientX}, Mouse Y: ${mouseEvent.clientY}`);
+}
+
+function draw(){
+    BRUSH.beginPath();
+    BRUSH.arc(circleX, circleY, circleRadius, 0, 2 * Math.PI);
+    BRUSH.stroke();
+}
