5bbca36920189ead398c191d730e8b44f3497685
Ian Choy <boidiamond69420@gmail.com>
2026-06-12 14:38:14 +0800

    Updated function naming to avoid conflicts

--- assets/script/Day 2/wiresTask.js
+++ assets/script/Day 2/wiresTask.js
@@ -260,7 +260,7 @@ function mouseCollision(rgbX, rgbY){
 /**
  * Draws the wires on the Canvas
  */
-function draw(){
+function drawWires(){
     BRUSH.beginPath();
     BRUSH.arc(redStartX, redStartY, circleRadius, 0, 2 * Math.PI);
     BRUSH.strokeStyle = "red";
