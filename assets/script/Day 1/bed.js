ffd1428c826b6aefd54acfaee13cbd1dd946f858
marcusyeah <marcus.alan.kim@gmail.com>
2026-06-12 02:01:33 -0400

    Create bed day end code

--- assets/script/Day 1/bed.js
+++ assets/script/Day 1/bed.js
@@ -2,3 +2,30 @@
 
 'use strict';
 
+//create variable to determine if day 1 has ended yet
+let day1End = false;
+
+//create variable to check if player has gone to bed
+let bedInteract = false;
+
+
+//check if player is within bed collision
+if (_){
+    //check if player interacted with bed
+    if (check('interact')){
+        bedInteract = true;
+    }
+
+}
+
+
+
+
+
+
+//check if player did the final going to bed task
+if (bedInteract == true){
+
+    day1End = true;
+}
+
