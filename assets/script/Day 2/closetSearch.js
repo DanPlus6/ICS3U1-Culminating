ec095ac850faf5963044aeee945b00507d936ca5
Ian Choy <boidiamond69420@gmail.com>
2026-06-14 17:15:02 +0800

    Added doors and background for the closet

--- assets/script/Day 2/closetSearch.js
+++ assets/script/Day 2/closetSearch.js
@@ -14,8 +14,25 @@ CANVAS.height = CANVAS.clientHeight;
 //get the graphics content from the Canvas for drawing directions
 const BRUSH = CANVAS.getContext('2d');
 
-//get the monster image
+//get the closet images
 const BACKGROUND_IMAGE = document.createElement('img');
 BACKGROUND_IMAGE.src = '../../img/closetBG.png';
 
-let leftDoor
+const LDOOR_IMAGE = document.createElement('img');
+LDOOR_IMAGE.src = '../../img/closetLDoor.png';
+
+const RDOOR_IMAGE = document.createElement('img');
+RDOOR_IMAGE.src = '../../img/closetRDoor.png';
+
+let backgroundX = -CANVAS.width/2 + 27;
+
+function closetDraw(){
+    BRUSH.drawImage(BACKGROUND_IMAGE, backgroundX, -10, CANVAS.width, CANVAS.height + 20);
+    BRUSH.drawImage(LDOOR_IMAGE, 0, 0, CANVAS.width/2 - 10, CANVAS.height);
+    BRUSH.drawImage(RDOOR_IMAGE, CANVAS.width/2 + 10, 0, CANVAS.width/2 - 10, CANVAS.height);
+}
+
+
+function startCloset(){
+    closetDraw();
+}
