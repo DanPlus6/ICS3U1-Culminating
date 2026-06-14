2d40eeeb3b9905b6e6bcd8214faf9839052fe259
Ian Choy <boidiamond69420@gmail.com>
2026-06-15 05:40:20 +0800

    Added comments for the closetSearch

--- assets/script/Day 2/closetSearch.js
+++ assets/script/Day 2/closetSearch.js
@@ -24,15 +24,58 @@ LDOOR_IMAGE.src = '../../img/Day2Img/closetLDoor.png';
 const RDOOR_IMAGE = document.createElement('img');
 RDOOR_IMAGE.src = '../../img/Day2Img/closetRDoor.png';
 
+//get the jumpscare image
+const JUMPSCARE_IMAGE = document.createElement('img');
+JUMPSCARE_IMAGE.src = '../../img/Day3Img/3_Jumpscare.png';
+
+//get the x value of the closet
 let backgroundX = -CANVAS.width/2 + 27;
 
+//check if the closet background is moving
+let closetMove = false;
+
+//get when the jumpscare happens
+let jumpscareTiming = 575;
+
+/**
+ * Gets the closet event listeners
+ */
+function addClosetListeners(){
+    addEventListener('keydown', checkClosetDown);
+    addEventListener('keyup', checkClosetStop);
+}
+
+/**
+ * Draws the closet
+ */
 function closetDraw(){
+    BRUSH.clearRect(0, 0, CANVAS.width, CANVAS.height);
     BRUSH.drawImage(BACKGROUND_IMAGE, backgroundX, -10, CANVAS.width, CANVAS.height + 20);
     BRUSH.drawImage(LDOOR_IMAGE, 0, 0, CANVAS.width/2 - 10, CANVAS.height);
     BRUSH.drawImage(RDOOR_IMAGE, CANVAS.width/2 + 10, 0, CANVAS.width/2 - 10, CANVAS.height);
 }
 
+/**
+ * Checks if the player pressed the appropriate key and moves the background
+ * @param {KeyboardEvent} keydown information about the keyboard
+ */
+function checkClosetDown(keydown){
+    console.log(keydown.key);
+    //check if the key pressed is the correct key
+    if (keydown.key == 'ArrowLeft' || keydown.key == 'a'){
+        backgroundX+=5;
+        closetDraw();
+        //check if the background value is the same to trigger the jumpscare
+        if (backgroundX >= jumpscareTiming){
+            BRUSH.drawImage(JUMPSCARE_IMAGE, 0, 0, CANVAS.width, CANVAS.height);
+        }
+    }
+}
 
+/**
+ * Start the game
+ */
 function startCloset(){
     closetDraw();
+    addClosetListeners();
 }
